67011dbbebb62cab0e27582fc8081406764541d5
garve32 <garve32@gmail.com>
2024-07-07 17:47:55 +0900

    섹션5.리액트기본

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,14 @@
+import ProductList from '@/components/ProductList';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function ProductPage() {
-	return <div>hello next</div>;
+	return (
+		<div>
+			<h1>상품 목록</h1>
+			<ProductList></ProductList>
+		</div>
+	);
 }
 
 export default ProductPage;
